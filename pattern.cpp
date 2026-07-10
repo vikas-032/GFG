984340b55fc2a66836a344c0f494f76587b783a7
vikas-032 <7014.vikas@gmail.com>
2026-07-11 01:22:16 +0530

    finish of pattern

--- pattern.cpp
+++ pattern.cpp
@@ -152,3 +152,54 @@ int main(){
 
 
 */
+
+/*
+
+//Hallow Diamond Pattern but the code is error
+#include<iostream>
+using namespace std;
+int main() {
+	int n=5;
+	//top
+    for(int i=0; i<n; i++){
+        //space1 
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        cout<<"*";
+       if(i !=0){
+           //space2
+           for(int j=0; j<2*i-1; j++){
+               cout<<" ";
+           }
+           cout<<"*";
+
+        
+         cout<<endl;
+       }
+       
+       
+       for(int i=0 ; i<n; i++){
+    //space
+    for(int j=0; j<i+1; j++){
+        cout<<"";
+    }
+    cout<<"*";
+}
+if(i !=n-1){
+    //spaces
+    for(int j=0; j<2*(n-i)-5; j++){
+        cout<<" ";
+    }
+    cout<<"*";
+}
+       cout<<endl;
+    }
+
+	return 0;
+}
+
+*/
+
+
+
