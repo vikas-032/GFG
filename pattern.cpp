9c64dfc8a97ad8887b29065a93cf60c739ea4f1e
vikas-032 <7014.vikas@gmail.com>
2026-07-10 16:02:45 +0530

    pyramid pattern

--- pattern.cpp
+++ pattern.cpp
@@ -85,5 +85,70 @@ int main(){
 2 3 
 4 5 6 
 7 8 9 10 
+*/
+
+
+/*
+Inverted triangle pattern
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    int num=1;
+    for(int i=0; i<n; i++){
+        for(int j=0;j<i; j++){
+            cout<<" ";
+        }
+        //nums
+        for(int j=0; j<n-i; j++){
+            cout<<(i+1);
+        
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+1111
+ 222
+  33
+   4
+
+
+
+*/
+/*
+Pyramid pattern
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        //num
+        for(int j=1; j<=i+1; j++){
+            cout<<j;
+        }
+        //num2
+        for(int j=i ; j>=1; j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+   1
+  121
+ 12321
+1234321
+
 
 */
