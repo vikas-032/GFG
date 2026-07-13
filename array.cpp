057eb443bf129293df65acc80b0e9528438a946f
vikas-032 <7014.vikas@gmail.com>
2026-07-13 18:39:08 +0530

    inital commit

--- array.cpp
+++ array.cpp
@@ -1,3 +1,5 @@
+/*
+//  Q1 Smallest number using loop in Array?
 #include<iostream>
 using namespace std;
 int main(){
@@ -13,4 +15,24 @@ if(nums[i]<smallest){
 }
 cout<<"smallest" <<smallest <<endl;
 return 0;
+}
+*/
+
+// Q2 second part of finding smallest and largest at same time?
+
+#include<iostream>
+using namespace std;
+int main(){
+    int nums[]={5, 10, 66, 33, -44, -88};
+    int size=6;
+    int smallest=INT_MAX;
+    int largest=INT_MIN;
+    for(int i=0; i<size; i++){
+        smallest=min(nums[i], smallest);
+        largest=max(nums[i], largest);
+
+    }
+    cout<<"smallest"<<smallest<<endl;
+    cout<<"largest"<<largest<< endl;
+    return 0;
 }
